8b0ecd888710414617dcfd0f1cd78ca9cb3ddc0b
Junhong Liao <junhongliao77@gmail.com>
2024-01-16 07:48:27 -0500

    Updated fastapi example, requirements, and further auth.py development

--- auth.py
+++ auth.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, Request, HTTPException
 from authlib.integrations.starlette_client import OAuth
 from dotenv import load_dotenv
+from starlette.middleware.sessions import SessionMiddleware
 import os
 
 app = FastAPI()
+
+# load environment variables using dotenv (onto virtual 'system', for os to access)
 load_dotenv()
+
+# pull secret key
+SECRET_KEY = os.getenv("GOOGLE_CLIENT_SECRET")
+
+# add middleware, set secret key
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
+
 # OAuth setup
 oauth = OAuth()
 oauth.register(
@@ -22,16 +32,17 @@ oauth.register(
     }
 )
 
-"""
-async allows FastAPI to handle multiple requests concurrently
-when waiting on IO-bound operations (db call), frees up event loop to handle other requests
-However,if operation is bounded by CPU (very complex), async will not necessarily provide benefits...
-"""
 # redirect to Google OAuth
 @app.get("/login")
 async def login(request: Request):
     redirect_uri = request.url_for('auth')
     return await oauth.google.authorize_redirect(request, redirect_uri)
+
+"""
+async allows FastAPI to handle multiple requests concurrently
+when waiting on IO-bound operations (db call), frees up event loop to handle other requests
+However,if operation is bounded by CPU (very complex), async will not necessarily provide benefits...
+"""
 
 # OAuth callback route
 @app.get("/auth")
