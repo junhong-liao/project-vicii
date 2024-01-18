40581c017be12fd92a5eca33e9e13fa377cfd894
junhongliao77@gmail.com <junhongliao77@gmail.com>
2024-01-18 07:51:31 -0500

    Refactored auth microservice

--- auth.py
+++ auth.py
@@ -1,74 +1,24 @@
-from fastapi import FastAPI, Request, HTTPException
-from authlib.integrations.starlette_client import OAuth
+from fastapi import FastAPI
+from starlette.requests import Request
+from fastapi_sso.sso.google import GoogleSSO
 from dotenv import load_dotenv
-from starlette.middleware.sessions import SessionMiddleware
 import os
 
 app = FastAPI()
-
-# load environment variables using dotenv (onto virtual 'system', for os to access)
 load_dotenv()
 
-# pull secret key
-SECRET_KEY = os.getenv("GOOGLE_CLIENT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID", None)
+CLIENT_SECRET = os.getenv("CLIENT_SECRET", None)
 
-# add middleware, set secret key
-app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
+google_sso = GoogleSSO(CLIENT_ID, CLIENT_SECRET, "http://localhost:8000/google/callback")
 
-# OAuth setup
-oauth = OAuth()
-oauth.register(
-    name='google',
-    # I have to set these system variables
-    client_id=os.getenv('GOOGLE_CLIENT_ID'),
-    client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://accounts.google.com/o/oauth2/v1',
-    jwks_uri="https://www.googleapis.com/oauth2/v3/certs",
-    client_kwargs={
-        'scope': 'openid email profile',
-    }
-)
+@app.get("/google/login")
+async def google_login():
+    with google_sso:
+        return await google_sso.get_login_redirect()
 
-# redirect to Google OAuth
-@app.get("/login")
-async def login(request: Request):
-    redirect_uri = request.url_for('auth')
-    return await oauth.google.authorize_redirect(request, redirect_uri)
-
-"""
-async allows FastAPI to handle multiple requests concurrently
-when waiting on IO-bound operations (db call), frees up event loop to handle other requests
-However,if operation is bounded by CPU (very complex), async will not necessarily provide benefits...
-"""
-# OAuth callback route
-# @app.get("/auth")
-# async def auth(request: Request):
-#     try:
-#         token = await oauth.google.authorize_access_token(request)
-#         user = await oauth.google.parse_id_token(request, token)
-#         return {"user": user}
-#     except Exception as e:
-#         # 401 unauthorized, convert exception to str
-#         raise HTTPException(status_code=401, detail=str(e))\
-
-# debug version
-@app.get("/auth")
-async def auth(request: Request):
-    try:
-        token = await oauth.google.authorize_access_token(request)
-        # print(token)
-        # user = await oauth.google.parse_id_token(request, token['id_token'])
-        user = await oauth.google.parse_id_token(request, token)
-        return {"user": user}
-    except Exception as e:
-    #     # 401 unauthorized, convert exception to str
-        raise HTTPException(status_code=401, detail=str(e))
-
-# testing endpoint
-@app.get("/greet/{name}")
-def greet(name: str):
-    return {"message": f"Hello, {name}!"}
+@app.get("/google/callback")
+async def google_callback(request: Request):
+    with google_sso:
+        user = await google_sso.verify_and_process(request)
+    return user
