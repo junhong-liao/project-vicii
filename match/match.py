41b1685bd1f4e0c2b2b432f5a1353255012062a7
junhongliao77@gmail.com <junhongliao77@gmail.com>
2024-02-12 12:11:44 -0500

    feature/match

--- match/match.py
+++ match/match.py
@@ -4,11 +4,11 @@ from db.operations import get_user_by_email, add_user
 """
 Match users based on their elo.
 
-ELO = A function of
+ELO = {
     How well you communicate (practice)
     What you know (knowledge)
     Professional conduct 
-
+}
 
 Pro Users: Ability to match people all day (show active users)
 
@@ -19,14 +19,30 @@ Create Eastern Version (9am, 8pm, 9pm)
 
 Best times?
 
-Option 1: {6am, 7am, 8am, 9am}
-Option 2: {6pm, 7pm, 8pm, 9pm}
+ET and PT cover both largest tech hubs.
+
+These are signups:
+Option 1: {6am, 7am, 8am, 9am} ET, PT
+Option 2: {6pm, 7pm, 8pm, 9pm} ET, PT
 
 
-If there are online users that are += 10 
-If there is someone anyone in the same 'rating bucket', you can be matched
 
+Matching Process:
+a) Match everyone as closely as possible, with someone else who has done a similar # of mocks
+b) For people who don't have a good match, tell them why, and still let them decide whether or not its cool
 
+Make matching algorithm
+    Partition
+    Closest Match
+    Personality Type
+    Growth Needs (someone who's good at xyz)
 
+Decide when to run it
+    Pro subscription: interview all day
+        This is because power users will self select themselves and be on all the time
+        + Rubrics, direct feedback
+        + Auto question bank
+        + People pay for how easy it is to access
+    Base: interview 2x a day.
 
 """
